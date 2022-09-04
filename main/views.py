634dca57b4a3e286ad48823386e2449980a01100
Harsh <73024251+Harsh-2200@users.noreply.github.com>
2022-09-05 01:02:19 +0530

    image and bugs

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render
 from .forms import ImageUpload
 from tensorflow.keras.applications.resnet50 import ResNet50
@@ -7,39 +8,36 @@ import numpy as np
 
 
 def handle_file(f):
-    with open('img.png' , 'wb+') as dest:
+    with open('img.png', 'wb+') as destination:
         for chunk in f.chunks():
-            dest.write(chunk)
+            destination.write(chunk)
+
 
 def home(request):
     context = {}
-    return render(request , 'main/home.html' , context )
+    return render(request, 'imupload/home.html', context)
+
 
 def imgprocess(request):
-    form = ImageUpload(request.POST , request.FILES)
+    form = ImageUpload(request.POST, request.FILES)
     if form.is_valid():
         handle_file(request.FILES['image'])
 
-        model = ResNet50(weights = 'imagenet')
+        model = ResNet50(weights='imagenet')
         img_path = 'img.png'
-        img = image.load_img(img_path , target_size = (244,244))
+        img = image.load_img(img_path, target_size=(224, 224))
         x = image.img_to_array(img)
-        x = np.exapnd_dims(x  , axis=0)
-        x = preprocessing_input(x)
-
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
         preds = model.predict(x)
+        print('Predicted:', decode_predictions(preds, top=3)[0])
 
-        print('Predict : ' , decode_predictions(preds , top = 3))[0]
-
-        htm = decode_predictions(preds , top= 3)[0]
+        htm = decode_predictions(preds, top=3)[0]
         result = []
-
-        for i in htm :
-            result.append((i[1] , np.round(i[2]*100 , 2 )))
-        return render(request , 'main/result.html' , {'result':result} )
-    return render(request , 'main/home.html')
-
-
+        for i in htm:
+            result.append((i[1], np.round(i[2]*100, 2)))
+        return render(request, 'main/result.html', {'result': result})
+    return render(request, 'main/home.html')
 
 
 
--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bpq-f!^y6ws28yhd9wdr!r-f2a8cht-+pos0eva7s$98k)+8d*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
